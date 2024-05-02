5e187c2f896fd26eecd45556dde84c2c40432aee
Alisson Fernandes <alisson.vinicius@cohros.com.br>
2024-05-01 22:13:51 -0300

    first commit

---
+++ brazilian-salaries/src/components/MinimumSalaryReal.vue
@@ -0,0 +1,20 @@
+<script setup>
+  import Chart from 'chart.js/auto';
+  import BarChart from '@/components/charts/BarChart.vue'
+  import datasetSalaries from '@/assets/datasets/minimumSalariesReal.json';
+
+  const data = {
+    labels: Object.keys(datasetSalaries),
+    datasets: [{
+      label: 'Reais R$',
+      data: Object.values(datasetSalaries),
+      borderWidth: 1
+    }]
+  };
+</script>
+
+<template>
+  <BarChart :data="data" />
+</template>
+
+<style scoped></style>
---
+++ brazilian-salaries/src/components/charts/BarChart.vue
@@ -0,0 +1,28 @@
+<script>
+import Chart from 'chart.js/auto';
+
+export default {
+  props: ['data'],
+  mounted() {
+    const ctx = document.getElementById('barChart');
+
+    new Chart(ctx, {
+      type: 'bar',
+      data: this.data,
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
+}
+</script>
+
+<template>
+  <canvas id="barChart"></canvas>
+</template>
+
+<style scoped></style>
