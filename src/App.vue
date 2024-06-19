f5f4a313cc8e6e9464bdc98ea368de177b559959
Alisson Fernandes <alissonvinicios04@gmail.com>
2024-06-19 18:00:27 -0300

    [story] changed reponsivity

--- src/App.vue
+++ src/App.vue
@@ -8,16 +8,16 @@ export default {
   <main class="container-fluid pt-3">
     <DefaultCard :title="'Salário mínimo (Real R$)'">
       <div class="row">
-        <div class="col-6 p-2">
+        <div class="col-sm-12 col-lg-6 p-2">
           <MinimumSalaryReal />
         </div>
-        <div class="col-6 p-2">
+        <div class="col-sm-12 col-lg-6 p-2">
           <MinimumSalaryRealPercentage />
         </div>
-        <div class="col-6 p-2">
+        <div class="col-sm-12 col-lg-6 p-2">
           <MinimumSalaryRealAverageGrowth />
         </div>
-        <div class="col-6 p-2">
+        <div class="col-sm-12 col-lg-6 p-2">
           <MinimumSalaryRealArithmeticSequence />
         </div>
       </div>
