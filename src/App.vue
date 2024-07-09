d8a7b07205c623bfc9534238b0250b4b4022240a
Alisson Fernandes <alissonvinicios04@gmail.com>
2024-07-09 12:30:19 -0300

    [task] moved footer to a component

--- src/App.vue
+++ src/App.vue
@@ -45,21 +45,6 @@ export default {
       </div>
     </div>
 
-    <footer class="d-flex justify-content-center pb-2 pt-4 mt-5 border-top">
-      <a
-        href="https://github.com/alisson04/brazilian-minimum-salaries"
-        target="_blank"
-        class="text-muted nav-link m-3"
-      >
-        <i class="fa fa-2x fa-github" aria-hidden="true" title="Ver no GitHub"></i>
-      </a>
-      <a
-        href="https://www.linkedin.com/in/alisson-fernandes-6b761a143/"
-        target="_blank"
-        class="text-muted nav-link m-3"
-      >
-        <i class="fa fa-2x fa-linkedin-square" aria-hidden="true" title="Ver meu Linkedin"></i>
-      </a>
-    </footer>
+    <DefaultFooter />
   </main>
 </template>
